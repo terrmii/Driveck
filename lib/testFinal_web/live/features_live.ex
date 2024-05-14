48d1a9c92d7e8f5045a6ad2dc356f2b23cb63087
terrmii <arnoldbermell@gmail.com>
2024-05-14 22:40:41 +0100

    Logo change

--- lib/testFinal_web/live/features_live.ex
+++ lib/testFinal_web/live/features_live.ex
@@ -22,8 +22,8 @@ defmodule TestFinalWeb.FeaturesLive do
                 </svg>
             </button>
             <a href="https://flowbite.com" class="flex ms-2 md:me-24">
-              <img src="https://flowbite.com/docs/images/logo.svg" class="h-8 me-3" alt="FlowBite Logo" />
-              <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Tisacloud</span>
+              <img src="/images/Driveck.png" class="h-8 me-3" alt="FlowBite Logo" />
+              <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Driveck</span>
             </a>
           </div>
           <div class="flex items-center">
--- lib/testFinal_web/live/index_live.ex
+++ lib/testFinal_web/live/index_live.ex
@@ -20,16 +20,16 @@ defmodule TestFinalWeb.IndexLive do
             </a>
             <div class="grid grid-cols-2 gap-2 pt-20">
             <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt="">
+                <img class="h-auto max-w-full rounded-lg" src="/images/photo-4.jpg" alt="Photo-1">
             </div>
             <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt="">
+                <img class="h-auto max-w-full rounded-lg" src="/images/photo-2.jpg" alt="Photo-2">
             </div>
             <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
+                <img class="h-auto max-w-full rounded-lg" src="/images/photo-3.jpg" alt="Photo-3">
             </div>
             <div>
-                <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="">
+                <img class="h-auto max-w-full rounded-lg" src="/images/photo-1.jpg" alt="Photo-4">
             </div>
         </div>
         </div>
@@ -64,8 +64,8 @@ defmodule TestFinalWeb.IndexLive do
         <img class="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image">
         <img class="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image">
         <div class="mt-4 md:mt-0">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Let's create more tools and ideas that brings us together.</h2>
-            <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Flowbite helps you connect with friends and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups.</p>
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Let's organise our ideas together.</h2>
+            <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Driveck helps you with your projects to make them even better.</p>
             <a href="/register" class="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
                 Get started
                 <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -78,7 +78,7 @@ defmodule TestFinalWeb.IndexLive do
       <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
       <div class="max-w-screen-md mb-8 lg:mb-16">
           <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for business teams like yours</h2>
-          <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+          <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Driveck we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
       </div>
       <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
           <div>
